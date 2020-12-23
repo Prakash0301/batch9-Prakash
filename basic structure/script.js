73bac433ca60891945a24d4d377defc168b14251
Prakash0301 <prakash.s030194@gmail.com>
2020-12-23 21:38:43 +0530

    updated

--- basic structure/script.js
+++ basic structure/script.js
@@ -1,3 +1,27 @@
-alert("hi hello")
+// //create array
+// var favcolors = ["red", "white", "black"]
+// favcolors[4] = "yellow"
+// favcolors[5] = "50"
+// console.log(favcolors);
 
-console.log("hi hello")
+// //create a object
+// var person = {
+//     name: "shankar",
+//     age: 25,
+//     gender: "Male",
+//     address: "india",
+// }
+// console.log(person);
+
+// create IF, If-else
+
+Var age =10 ;
+if (age>=18)
+{
+    console.log("your are 18+ so gohead");
+}
+else (age<=18)
+{
+    console.log("you are under 18 so not enough");
+}
+console.log(age);
