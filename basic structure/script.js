81b9657043d709333adee323db46d77677a079a7
Prakash0301 <prakash.s030194@gmail.com>
2020-12-23 23:47:10 +0530

    updated

--- basic structure/script.js
+++ basic structure/script.js
@@ -1,27 +1,107 @@
-// //create array
-// var favcolors = ["red", "white", "black"]
-// favcolors[4] = "yellow"
-// favcolors[5] = "50"
-// console.log(favcolors);
+//create array
+var favcolors = ["red", "white", "black"]
+favcolors[4] = "yellow"
+favcolors[5] = "50"
+console.log(favcolors);
 
-// //create a object
-// var person = {
-//     name: "shankar",
-//     age: 25,
-//     gender: "Male",
-//     address: "india",
-// }
-// console.log(person);
-
-// create IF, If-else
-
-Var age =10 ;
-if (age>=18)
-{
-    console.log("your are 18+ so gohead");
+//create a object
+var person = {
+    name: "shankar",
+    age: 25,
+    gender: "Male",
+    address: "india",
 }
-else (age<=18)
-{
-    console.log("you are under 18 so not enough");
+console.log(person);
+
+//If, If-else statement
+
+var mark = 85;
+if (mark >= 75) {
+    console.log('your are pass');
 }
-console.log(age);
+else {
+    console.log('your are failed!');
+}
+
+//If, else-if statement
+var whoIsHere = 'not anyone'
+if (whoIsHere == 'student') {
+    console.log('hi i am a student');
+}
+else if (whoIsHere == 'teacher') {
+    console.log('hi i am a teacher');
+}
+else if (whoIsHere == 'headmaster') {
+    console.log('hi i am a headmaster');
+}
+else {
+    console.log('dont no you are');
+}
+
+//for loop statement
+
+for (var i = 0; i <= 6; i++) {
+    if (i % 2 == 0) {
+        console.log("EvenNumber#", i);
+    }
+}
+
+//while loop statement
+
+var i = 1;
+while (i <= 10) {
+    if (i % 2 !== 0) {
+        console.log('Odd Number#', i);
+    }
+    i++;
+}
+
+//Do while loop statement
+
+var i = 1;
+while (i <= 10) {
+    if (i % 2 !== 0) {
+        console.log('Odd Number#', i);
+    }
+    i++;
+}
+do {
+    console.log(i);
+    if (i % 2 !== 0) {
+        console.log('do while Number', i)
+    }
+    i++;
+} while (i <= 10)
+
+
+//Switch case, break , continue statement
+var grade = "a";
+switch (grade) {
+    case "a":
+        console.log("super grade");
+        break;
+    case "b":
+        console.log("Excellent");
+        break;
+    case "d":
+        console.log("Good");
+        break;
+    case "e":
+        console.log("Just pass");
+        break;
+    case "f":
+        console.log("Failed!");
+        break;
+    default:
+        console.log("Unknow grade");
+}
+
+// continue statement
+
+for (var i = 0; i < 6; i++) 
+{
+    if (i == 3)
+        continue;
+    console.log("EvenNumber#", i);
+
+}
