a2264042c2743bf3db0d05b366ad0242ac523a53
Prakash0301 <prakash.s030194@gmail.com>
2020-12-31 08:27:34 +0530

    updated

--- user management login/new.js
+++ user management login/new.js
@@ -1,5 +1,25 @@
 function login(){
     debugger;
-    console.log("username: " + $ ("username").val());
-    console.log("password: " + $ ("password").val());
+    console.log("username: " + $("username").val());
+    console.log("password: " + $("password").val());
+}
+function CreateAccount(){
+    debugger;
+
+}
+function switchForms(formType){
+    if (formType === "login"){
+        $("#pagetitle").text("loginform");
+        // $("#loginform").removeclass("hide");
+        // $("#CreateAccount").addClass("hide");
+        $("#loginform").show();
+        $("#CreateAccount").hide();
+    }
+    else{
+        $("#pagetitle").text("Create account form");
+        // $("#loginform").addclass("hide");
+        // $("#CreateAccount").removeClass("hide");
+        $("#loginform").hide();
+        $("#CreateAccount").show();
+    }
 }
