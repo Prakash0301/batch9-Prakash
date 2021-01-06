b387205f209d4deac31e7ae25fe58336e54498a8
Prakash0301 <prakash.s030194@gmail.com>
2021-01-06 10:55:14 +0530

    changes updated

--- usermanagement/test.js
+++ usermanagement/test.js
@@ -1,24 +1,25 @@
             var user = {};
-        //     var firstName = document.forms['createform']['firstName']
-        //     var lastName = document.forms['createform']['lastName']
+            var firstName = document.forms['createform']['firstName']
+            var lastName = document.forms['createform']['lastName']
             
-        //     var firstName_error = document.getElementById('firstName_error');
-        //     var lastName_error = document.getElementById('lastName_error');
+            var firstName_error = document.getElementById('firstName_error');
+            var lastName_error = document.getElementById('lastName_error');
             
-        //     function validated(){
-        //     if(email.value.length < 9){
-        //        email.style.border = "1px solid red";
-        //        email_error.style.display = "block"
-        //        email.foucs();
-        //        return false;
-        //     }
-        //     if(password.value.length < 9){
-        //     password.style.border = "1px solid red";
-        //     password_error.style.display = "block"
-        //     password.foucs();
-        //     return false;
-        //     }
-        //  }
+            function validated(){
+            if(email.value.length < 9){
+               email.style.border = "1px solid red";
+               email_error.style.display = "block"
+               email.foucs();
+               return false;
+            }
+            if(password.value.length < 9){
+            password.style.border = "1px solid red";
+            password_error.style.display = "block"
+            password.foucs();
+            return false;
+            }
+            return false;
+         }
             var usersList = [];   
             var userIdCount = 1;
             function checkPass(){
